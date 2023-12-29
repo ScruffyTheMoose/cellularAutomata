a53084084f6caf1a8f700887d9dcd725c54536e9
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-12-29 15:24:40 -0700

    refactor and rename automata main file

--- app/automata.py
+++ app/automata.py
@@ -2,45 +2,11 @@ import time
 import pygame
 import numpy as np
 
-COLOR_BG = (0, 255, 0)  # will show green if missing cells
-COLOR_GRID = (40, 40, 40)
-COLOR_DIE = (255, 100, 100)
-COLOR_ALIVE = (255, 255, 255)
-DISPLAY_SIZE = (1000, 800)
-CELL_WIDTH = 10
+import rulesets as rs
+from constants import *
 
 
-def update(screen, cells, size=CELL_WIDTH, with_progress=False):
-    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
-
-    for row, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[row - 1: row + 2, col -
-                       1: col + 2]) - cells[row, col]
-        color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE
-
-        if cells[row, col] == 1:
-            if alive < 2 or alive > 3:
-                if with_progress == True:
-                    color = COLOR_DIE
-
-            elif 2 <= alive <= 3:
-                updated_cells[row, col] = 1
-                if with_progress:
-                    color = COLOR_ALIVE
-
-        else:
-            if alive == 3:
-                updated_cells[row, col] = 1
-                if with_progress:
-                    color = COLOR_ALIVE
-
-        pygame.draw.rect(screen, color, (col * size,
-                         row * size, size, size))
-
-    return updated_cells
-
-
-def main():
+def main(update=rs.life):
     pygame.init()
     screen = pygame.display.set_mode(DISPLAY_SIZE)
 
