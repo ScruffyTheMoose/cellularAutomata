748bfa7cc7a0985c13612e3903244e5be6206948
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2023-12-29 18:02:03 -0700

    add world randomization tool to game

--- app/automata.py
+++ app/automata.py
@@ -1,12 +1,13 @@
 import time
 import pygame
 import numpy as np
+import numpy.random as npr
 
 import rulesets as rs
 from constants import *
 
 
-def main(rule="34life", update=rs.update):
+def main(rule="2x2", update=rs.update):
     pygame.init()
     screen = pygame.display.set_mode(DISPLAY_SIZE)
 
@@ -26,17 +27,25 @@ def main(rule="34life", update=rs.update):
                 pygame.quit()
                 return
 
+            # pause/unpause
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
-                    update(screen=screen, cells=cells, rule=rule)
-                    pygame.display.update()
 
+                # add random live cells
+                if event.key == pygame.K_r:
+                    rand_cells = npr.choice([0, 1], size=(
+                        DISPLAY_SIZE[1] // CELL_WIDTH, DISPLAY_SIZE[0] // CELL_WIDTH), p=[2.5/3, 0.5/3])
+                    cells = np.logical_or(rand_cells, cells).astype(int)
+
+            # draw live cells with mouse
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cells[pos[1] // CELL_WIDTH, pos[0] // CELL_WIDTH] = 1
-                update(screen=screen, cells=cells, rule=rule)
-                pygame.display.update()
+
+            # update world after inputs
+            update(screen=screen, cells=cells, rule=rule)
+            pygame.display.update()
 
         screen.fill(COLOR_GRID)
 
